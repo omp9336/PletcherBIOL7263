26004511e72a22e8083fca279c671420b906a54a
omp9336 <omp9336@utulsa.edu>
2022-10-05 17:29:41 -0500

    finished 4.3

--- scripts/assignment 4.R
+++ scripts/assignment 4.R
@@ -6,28 +6,37 @@ glimpse(MBT_ebird)
 
 Ebird_cut1 <- select(MBT_ebird, count_tot, year)
 
-glimpse(Ebird_cut1)
+year_counts <- group_by(Ebird_cut1, year)
 
-arrange (Ebird_cut1, by = count_tot)
+summarize(year_counts, sum(count_tot))
 
-arrange (Ebird_cut1, by = desc(count_tot))
+#2014 has the highest observation of individual birds with a total count of 211249.
 
-# 2020 is the year with the most individual birds of 3154
-
-library(tidyverse)
+as_tibble(MBT_ebird)
 
 glimpse(MBT_ebird)
 
-Ebird_cut2 <- select(MBT_ebird, scientific_name, year)
+Year_species <- select(MBT_ebird, scientific_name, year)
 
-glimpse(Ebird_cut2)
+Year_species %>%
+  filter(year == 2014) %>%
+  group_by(scientific_name)
 
-Ebird_cut3 <- filter(Ebird_cut2, year == 2020)
+#In 2014 there were 210 species observed.
 
-glimpse(Ebird_cut3)
+as_tibble(MBT_ebird)
 
-Ebird_cut3 %>%
-  
+state_cut1 <- select(MBT_ebird, location, common_name, count)
+
+glimpse(state_cut1)
+
+blackbird_count <- filter(state_cut1, common_name == "Red-winged Blackbird")
+
+blackbird_count %>%
+  group_by(location) %>%
+  summarize (sum(count))
+
+#Missouri had the most total observations of 596 red-winged blackbirds.
 
 
 
