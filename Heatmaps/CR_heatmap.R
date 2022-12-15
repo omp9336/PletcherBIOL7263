842159ddfe8c696073f030ecbfa76263286f4210
omp9336 <omp9336@utulsa.edu>
2022-12-14 22:24:39 -0600

    tropical ecology stuff

--- Heatmaps/CR_heatmap.R
+++ Heatmaps/CR_heatmap.R
@@ -1,9 +1,18 @@
 library(devtools)
 install_github("jokergoo/ComplexHeatmap")
 3
-
+install.packages("ComplexHeatmap")
+library(ComplexHeatmap)
 
 Species_data <-read.csv("Heatmaps/species_data.csv")
 
 species_data1 <- as.matrix(Species_data [, -1])
 rownames(species_data1) <- Species_data[,1]
+
+heatmap(species_data1)
+
+library(circlize)
+
+heatmap(species_data1, name = "mat", cluster_rows = FALSE)
+
+
